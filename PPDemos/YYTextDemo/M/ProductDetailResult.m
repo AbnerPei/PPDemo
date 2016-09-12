82b66dc543a5844afd225c06bdc11e2a7c32623c
chinesemanbobo <chinesemanbobo@163.com>
2016-09-12 09:10:33 +0800

    add yytext

---
+++ PPDemos/YYTextDemo/M/ProductDetailResult.m
@@ -0,0 +1,13 @@
+//
+//  ProductDetailResult.m
+//  PPDemos
+//
+//  Created by Abner on 16/9/9.
+//  Copyright © 2016年 PPAbner. All rights reserved.
+//
+
+#import "ProductDetailResult.h"
+
+@implementation ProductDetailResult
+
+@end
